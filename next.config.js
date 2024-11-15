1577d4f6190154e3824b966992d0451d30741d1d
Hannan <hannansshah2004@gmail.com>
2024-11-14 22:23:55 -0500

    nexts

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export', // Static export configuration
   images: {
-    unoptimized: true,
+    unoptimized: true, // Unoptimized images (optional)
   },
 };
 
